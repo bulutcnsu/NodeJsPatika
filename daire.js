139c1dd78ae435823bf3014ec56443696775ad2a
bulutcnsu <60491413+bulutcnsu@users.noreply.github.com>
2024-12-15 00:51:17 +0300

    Delete daire.js

--- daire.js
+++
@@ -1,9 +0,0 @@
-let pi = 3.14159265358979323846;
-const argument = process.argv.slice(2);
-
-function findArea(r) {
-  
-  var Alan = pi*r*r;
-  console.log("Yarıçapı " + r + "  Olan Dairenin Alanı: " + Alan)
-  }
-  findArea(argument[0]*1);
