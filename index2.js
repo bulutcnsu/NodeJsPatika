7f003ec03e9c823a38f44212cefc390ddccbe059
bulutcnsu <60491413+bulutcnsu@users.noreply.github.com>
2024-12-15 01:16:24 +0300

    Delete index2.js

--- index2.js
+++
@@ -1,7 +0,0 @@
-
-const circleProcess = require('./circle.js');
-
-const radius = 5;
-
-console.log("Area of circle: " +  circleProcess.circleArea(5) +  
-    "\tCircumference : " + circleProcess.circleCircumference(5));
